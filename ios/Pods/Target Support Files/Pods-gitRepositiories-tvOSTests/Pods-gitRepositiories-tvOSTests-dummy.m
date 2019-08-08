c7e47455260467b7cc2d5dcb46cba0e32fe54700
Nicolau Atala <nicolau.atala@hotmail.com>
2019-08-08 20:40:28 -0300

    Feat: Initial commit

---
+++ ios/Pods/Target Support Files/Pods-gitRepositiories-tvOSTests/Pods-gitRepositiories-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gitRepositiories_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_gitRepositiories_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-gitRepositiories/Pods-gitRepositiories-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gitRepositiories : NSObject
+@end
+@implementation PodsDummy_Pods_gitRepositiories
+@end
